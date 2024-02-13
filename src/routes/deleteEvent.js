55b9b4c68fff0f5f9a77b973d6987a32783100c3
Himanshu Bharuka <bharuka.himanshu@gmail.com>
2024-02-13 22:14:23 +0530

    Delete event with authorisation

--- src/routes/deleteEvent.js
+++ src/routes/deleteEvent.js
@@ -1,53 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Event = require('../models/events');
-const User = require('../models/user');
 const isAuthenticated = require('../middleware/authMiddleware');
+const deleteEvent = require('../controllers/deleteEvent')
 
 // Route to handle deleting an event
-router.delete('/delete/:eventId', isAuthenticated, async (req, res) => {
-    try {
-        const { eventId } = req.params;
-
-        // Find the event by ID
-        const event = await Event.findById(eventId);
-
-        if (!event) {
-            return res.status(404).json({ error: 'Event not found' });
-        }
-
-        // Delete the event from all users' arrays
-        const usersUpdatePromises = [];
-
-        // Remove event from users' eventsAttending array
-        usersUpdatePromises.push(User.updateMany(
-            { eventsAttending: eventId },
-            { $pull: { eventsAttending: eventId } }
-        ));
-
-        // Remove event from users' maybeAttending array
-        usersUpdatePromises.push(User.updateMany(
-            { maybeAttending: eventId },
-            { $pull: { maybeAttending: eventId } }
-        ));
-
-        // Remove event from users' eventInvitations array
-        usersUpdatePromises.push(User.updateMany(
-            { eventInvitations: eventId },
-            { $pull: { eventInvitations: eventId } }
-        ));
-
-        // Execute all update promises
-        await Promise.all(usersUpdatePromises);
-
-        // Finally, delete the event itself
-        await Event.findByIdAndDelete(eventId);
-
-        res.status(200).json({ message: 'Event deleted successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+router.delete('/delete/:eventId', isAuthenticated, deleteEvent);
 
 module.exports = router;
